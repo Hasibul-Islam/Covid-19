0e177bede0d11ad69107c423ebfcd352abb8fe94
Polok <polok.hasibul@gmail.com>
2020-07-07 06:39:00 +0600

    Added some visualization

--- Task1.py
+++ Task1.py
@@ -1,5 +1,20 @@
 n = input()
-for c in n:
-	if not c.isalnum():
-		n = n.replace(c,'')
-print(n)
+def Method1(n):
+	m = (n+'.')[:-1]
+	for c in m:
+		if not c.isalnum():
+			m = m.replace(c,'')
+	print(m)
+
+
+def Method2(n):
+	m = (n+'.')[:-1]
+	PERMITTED_CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-" 
+	for c in m:
+		if c not in PERMITTED_CHARS:
+			m = m.replace(c,'')
+	print(m)
+
+
+Method1(n)
+Method2(n)
