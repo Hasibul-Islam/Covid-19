2fdb9e9f0e4784e1fcf101583d27d66fc614c8bf
Polok <polok.hasibul@gmail.com>
2020-07-09 09:02:21 +0600

    updated task

--- Task1.py
+++ Task1.py
@@ -1,5 +1,13 @@
+'''
+I have tried 2 methods here.
+'''
 n = input()
 def Method1(n):
+	'''
+	input: takes a parameter as string. n is the name of the string.
+	operation: removes every character that are not alphanumeric. Used in built alphanumeric method.
+	output: returns the string after removing the non alphanumeric characters.
+	'''
 	m = (n+'.')[:-1]
 	for c in m:
 		if not c.isalnum():
@@ -8,6 +16,12 @@ def Method1(n):
 
 
 def Method2(n):
+	'''
+	input: takes a parameter as string. n is the name of the string.
+	operation: removes every character that are not alphanumeric. Used a list of permited characters so that I can check whether
+	the characters are accepted or not.
+	output: returns the string after removing the non alphanumeric characters.
+	'''
 	m = (n+'.')[:-1]
 	PERMITTED_CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-" 
 	for c in m:
@@ -16,5 +30,5 @@ def Method2(n):
 	return m
 
 
-print(Method1(n))
-print(Method2(n))
+print(Method1(n)) #Call Method1
+print(Method2(n)) #Call Method2
--- Task2.py
+++ Task2.py
@@ -1,7 +1,22 @@
+'''
+Tried two methods here.
+'''
+
 def Method1(str1,str2):
+	'''
+	input: 2 parameters. str1,str2. str1 is the main string and str2 is the string to find in str1.
+	operation: simply returns the number of counts of occurence of str2 in str1 using built in count funtion.
+	output: returns an integer number: the number of occurence.
+	'''
 	return str1.count(str2)
 
 def Method2(str1,str2):
+	'''
+	input: 2 parameters. str1,str2. str1 is the main string and str2 is the string to find in str1.
+	operation: simply returns the number of counts of occurence of str2 in str1. Here checking whether a sequence of characters of
+	str1 are matched with characters of str2. 
+	output: returns an integer number: the number of occurence.
+	'''
 	cnt = 0
 	i = 0
 	while i<len(str1):
@@ -24,5 +39,5 @@ string_1  =  input()
 
 string_2 = input()
 
-print(Method1(string_1,string_2))
-print(Method2(string_1,string_2))
+print(Method1(string_1,string_2)) #call Method1
+print(Method2(string_1,string_2)) #call Method2
